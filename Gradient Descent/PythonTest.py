df24b811f7815d8a8ba82e75e0710fc7e8add59a
Hoàng Đình Hữu <81850325+IvanDominis@users.noreply.github.com>
2023-09-24 00:48:45 +0700

    Add files via upload

---
+++ Gradient Descent/PythonTest.py
@@ -0,0 +1,5 @@
+a=input('Nhap a: ')
+b=input('Nhap b: ')
+min=((a+b)-abs(a-b))/2
+max=((a+b)+abs(a-b))/2
+print(str(min) + str(max))
---
+++ Gradient Descent/main.py
@@ -0,0 +1,19 @@
+x =[2, 1, 5, 3, 4]
+y =[9, 5, 21, 13, 17]
+w, b = 10, 20
+m = 5
+while True:
+    Loss, wNew, bNew = 0, 0, 0
+    for i in range(5):
+        Loss += ((w*x[i] + b - y[i])**2)/(2*m)
+    if Loss <= 0.00000001:
+        break
+    for i in range(5):
+        wNew += (w * x[i] + b - y[i]) * x[i]
+        bNew += (w * x[i] + b - y[i]) / m
+    w -= 0.0001*wNew
+    b -= 0.0001*bNew
+print("y = " + str(w) + "x + "+ str(b))
+import matplotlib.pyplot as plt
+plt.plot([2,1,5,3,4],[9,5,21,13,17])
+plt.show()
